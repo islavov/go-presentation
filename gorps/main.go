8df6f5072afe5006e9eabf2c1bdd90bd6c54eb09
Ivelin Slavov <ivelin@uber.com>
2016-12-10 16:41:08 +0200

    Game working

--- gorps/main.go
+++ gorps/main.go
@@ -5,24 +5,38 @@ import (
 	"fmt"
 	"bufio"
 	"strings"
+	"math"
 )
+
+var SURRENDER = -1
+
+var ROCK = 0
+var PAPER = 1
+var SCISSORS = 2
+
+const MSG_WEAPON = "Choose your weapon (1: rock, 2:paper, 3:scissors): "
+
+const STATE_NEW= "new"
+const STATE_JOINED = "joined"
+const STATE_WAITING = "waiting"
+const STATE_PLAYING= "playing"
 
 
 type Player struct {
-	name string
-	action chan string
+	name    string
+	state   string
+	action  chan string
 	message chan string
 }
 
-
 func NewPlayer(name string) *Player {
-	return &Player{name: name, action: make(chan string), message: make(chan string)}
+	return &Player{name: name, action: make(chan string), message: make(chan string), state: STATE_NEW}
 }
 
 type Action struct {
 	player *Player
 	action string
-	param string
+	param  string
 }
 
 type Game struct {
@@ -30,32 +44,87 @@ type Game struct {
 	players []*Player
 }
 
-
-func NewGame() *Game{
+func NewGame() *Game {
 	var game = Game{players: []*Player{}, actions: make(chan Action)}
 	go game.cmdloop()
 	return &game
 }
 
+func msgPlayers(msg string, players ...*Player) {
+	for _, player := range players {
+		player.message <- msg
+	}
+}
+
+func handleAction(player *Player, action string) *int {
+	switch action{
+	case "1":
+		return &ROCK
+	case "2":
+		return &PAPER
+	case "3":
+		return &SCISSORS
+	case "-1":
+		return &SURRENDER
+	default:
+		player.message <- MSG_WEAPON
+		return nil
+	}
+}
+
+func gameLogic(decision1 int, decision2 int) bool {
+	if decision1 == decision2 {
+		return false
+	}
+	if math.Mod(float64(decision1 + 3) - float64(decision2), 3) == 1 {
+		return true
+	}
+	return false
+}
 
 func startMatch(game *Game, player1 *Player, player2 *Player) {
-	var decision1 string
-	var decision2 string
+	var decision1 *int
+	var decision2 *int
 
-	var msg = fmt.Sprintf("Duel between %s and %s. FIGHT!", player1.name, player2.name)
-	player1.message <- msg
-	player2.message <- msg
+	msgPlayers(MSG_WEAPON, player1, player2)
 
 	for {
 		select {
-		case decision1 = <-player1.action:
-			fmt.Printf("Hit player 1: %s", decision1)
-		case decision2 = <-player2.action:
-			fmt.Printf("Hit player 2: %s", decision2)
+		case action := <-player1.action:
+			decision1 = handleAction(player1, action)
+		case action := <-player2.action:
+			decision2 = handleAction(player2, action)
 		default:
-			if (decision1 != "" && decision2 != "") {
-				game.endMatch(player1, "win")
-				game.endMatch(player2, "lose")
+			var winner *Player
+			var loser *Player
+
+
+			if (decision1 != nil && decision2 != nil) {
+				if *decision1 == SURRENDER {
+					winner = player2
+				} else if *decision2 == SURRENDER {
+					winner = player1
+				} else if gameLogic(*decision1, *decision2) {
+					winner = player1
+					loser = player2
+				} else if gameLogic(*decision2, *decision1) {
+					winner = player2
+					loser = player1
+				} else {
+					msgPlayers("DRAW", player1, player2)
+					decision1 = nil
+					decision2 = nil
+				}
+
+			}
+
+			if winner != nil {
+				game.endMatch(winner, "win")
+			}
+			if loser != nil {
+				game.endMatch(loser, "lose")
+			}
+			if winner != nil || loser != nil {
 				return
 			}
 		}
@@ -72,29 +141,44 @@ func (game *Game) cmdloop() {
 		switch action.action {
 		case "addPlayer":
 			fmt.Printf("Joining: %s\n", action.player.name)
+			action.player.state = STATE_JOINED
 			game.players = append(game.players, action.player)
 		case "removePlayer":
+			if (action.player.state == STATE_PLAYING) {
+				action.player.action <- fmt.Sprintf("%d", SURRENDER)
+			}
 			if action.player == waiting {
 				waiting = nil
 			}
 			for idx, player := range game.players {
 				if player == action.player {
 					fmt.Printf("Leaving: %s\n", action.player.name)
-					game.players = append(game.players[:idx], game.players[idx+1:]...)
+					game.players = append(game.players[:idx], game.players[idx + 1:]...)
 					break
 				}
 			}
 		case "startMatch":
+
 			if waiting == nil {
 				waiting = action.player
-				waiting.message <- "waiting for another player..."
+				waiting.message <- "waiting for another player...\n"
+				waiting.state = STATE_WAITING
 			} else {
+
+				var msg = fmt.Sprintf("Duel between %s and %s. FIGHT!\n",
+					waiting.name, action.player.name)
+				waiting.message <- msg
+				action.player.message <- msg
+
+				waiting.state= STATE_PLAYING
+				action.player.state= STATE_PLAYING
+
 				go startMatch(game, action.player, waiting)
 				waiting = nil
 			}
 		case "endMatch":
-			action.player.message <- fmt.Sprintf("You %s", action.param)
-			//game.startMatch(action.player)
+			action.player.message <- fmt.Sprintf("You %s. Press any key to continue.\n", action.param)
+			action.player.state = STATE_JOINED
 		}
 
 	}
@@ -104,21 +188,17 @@ func (game *Game) addPlayer(player *Player) {
 	game.actions <- Action{player:player, action: "addPlayer", param:""}
 }
 
-
 func (game *Game) removePlayer(player *Player) {
 	game.actions <- Action{player:player, action: "removePlayer", param:""}
 }
-
 
 func (game *Game) startMatch(player *Player) {
 	game.actions <- Action{player:player, action: "startMatch", param:""}
 }
 
-
 func (game *Game) endMatch(player *Player, param string) {
 	game.actions <- Action{player:player, action: "endMatch", param:param}
 }
-
 
 func serve(conn net.Conn, game *Game) error {
 	defer conn.Close()
@@ -134,26 +214,30 @@ func serve(conn net.Conn, game *Game) error {
 	player := NewPlayer(strings.TrimSpace(name))
 	game.addPlayer(player)
 
-	go func () {
+	go func() {
 		for {
 			writer.Write([]byte(<-player.message))
 			writer.Flush()
 		}
 	}()
 
+	game.startMatch(player)
+
 	for {
-		game.startMatch(player)
-
-		for {
-			message, err := reader.ReadString('\n')
-			fmt.Println(strings.TrimSpace(message))
-			player.action <- message
-			if err != nil {
-				game.removePlayer(player)
-				return err
-			}
-
+		if (player.state == STATE_JOINED) {
+			game.startMatch(player)
 		}
+		message, err := reader.ReadString('\n')
+		if player.state == STATE_PLAYING && message != "" {
+			player.action <- strings.TrimSpace(message)
+		}
+
+		if err != nil {
+			game.removePlayer(player)
+			close(player.message)
+			return err
+		}
+
 	}
 
 	return nil
